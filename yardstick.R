7f532426382e7ecceed6386b3b2b9008df8aee59
MM <>
2025-04-21 20:46:17 +0100

    new yardstick pipeline

--- yardstick.R
+++ yardstick.R
@@ -1,27 +1,15 @@
-library(RefManageR)
-library(bibtex)
 library(jsonlite)
 library(httr)
 
 source("fun.R")
 source("credentials.R")
 
-bib <- ReadBib("./parameters/rawdata/yardstick_articles.bib")
-
-bib <- as.data.frame(bib)
-bib <- subset(bib, subset=bibtype!="Book", 
-        select=c("title", "abstract"))
-
-bib$title <- gsub("\\{|\\}", "", bib$title)
-bib$abstract <- gsub("\\n|\\t}", " ", bib$abstract)
-bib$abstract <- gsub("  *", " ", bib$abstract)
-rownames(bib) <- NULL 
-
-table(is.na(bib$title))
-table(is.na(bib$abstract))
-
-write(toJSON(bib), file="./parameters/rawdata/yarstick_articles.json")
-
+# Compute yardstick 
+bib <- read_csv("./parameters/rawdata/yarstick_articles_new.csv")
 str <- with(bib, paste0("Title: ", title, "\n Abstract: ", ifelse(is.na(abstract), "", abstract)))
 vec <- lapply(str, function(x) get_openai_embedding(x)) 
 write(toJSON(vec), "./parameters/yardstick.json")
+
+# Yardstick paper stack 
+bib <- subset(bib, select=c("year", "author", "title", "journal", "doi"))
+write(toJSON(bib), "./output/yardstick_papers.json")
