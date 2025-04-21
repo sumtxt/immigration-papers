d8a99b51284ae0ef8d5377445373026da5514f0d
MM <>
2025-04-21 22:21:19 +0100

    sorting of yardstick paper stack

--- yardstick.R
+++ yardstick.R
@@ -1,15 +1,18 @@
+library(readr)
 library(jsonlite)
 library(httr)
 
 source("fun.R")
 source("credentials.R")
 
+bib <- read_csv("./parameters/rawdata/yarstick_papers.csv")
+
 # Compute yardstick 
-bib <- read_csv("./parameters/rawdata/yarstick_articles_new.csv")
 str <- with(bib, paste0("Title: ", title, "\n Abstract: ", ifelse(is.na(abstract), "", abstract)))
 vec <- lapply(str, function(x) get_openai_embedding(x)) 
 write(toJSON(vec), "./parameters/yardstick.json")
 
 # Yardstick paper stack 
 bib <- subset(bib, select=c("year", "author", "title", "journal", "doi"))
+bib <- bib[order(bib$year*(-1)),]
 write(toJSON(bib), "./output/yardstick_papers.json")
